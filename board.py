eb1bdb68345484b9e4f7b7e02b6c3ada09b6a0c4
David Rubinstein <drubinstein91@gmail.com>
2016-09-17 14:19:00 -0400

    Added extra logic for castling

--- board.py
+++ board.py
@@ -2,15 +2,21 @@ from piece import *
 from colorama import Fore, Back, Style
 import sys
 
+class Reference(object):
+    def __init__(self, value):
+        self.value = value
+
 class Chess:
 
-    class ErrorCodes(Enum):
+    class ReturnCodes(Enum):
         NONE = 0 #No error
-        INVALID_MOVE_LENGTH          = 1 # The string used to make the move is of an invalid length
-        OUT_OF_BOUNDS_START_LOCATION = 2 # The string used to denote the start is out of bounds
-        OUT_OF_BOUNDS_END_LOCATION   = 3 # The string used to denote the end is out of bounds
-        INVALID_PIECE                = 4 # The piece you want to move is invalid
-        INVALID_MOVE                 = 5 # The move breaks the rules for the piece
+        CASTLE_LEFT                  = 1 # The move was a castle with the king moving left
+        CASTLE_RIGHT                 = 2 # The move was a castle with the king moving right
+        INVALID_MOVE_LENGTH          = 3 # The string used to make the move is of an invalid length
+        OUT_OF_BOUNDS_START_LOCATION = 4 # The string used to denote the start is out of bounds
+        OUT_OF_BOUNDS_END_LOCATION   = 5 # The string used to denote the end is out of bounds
+        INVALID_PIECE                = 6 # The piece you want to move is invalid
+        INVALID_MOVE                 = 7 # The move breaks the rules for the piece
 
     class BoardStates(Enum):
         NEUTRAL = 0
@@ -92,40 +98,63 @@ class Chess:
         #First parse the move into the current board position
         src, dest, err = parse_move(move)
 
-        if err != ErrorCodes.NONE:
+        if err != ReturnCodes.NONE:
             return err
 
         #Now attempt to make the move
         #Check if the piece is the correct color
-        sp = self.board[src[0]][src[1]]
-        dp = self.board[dest[0]][dest[1]]
+        sp = Reference(self.board[src[0]][src[1]])
+        dp = Reference(self.board[dest[0]][dest[1]])
         #This works because we have the concept of a colorless empty piece
         if sp.get_color() != self.current_player:
-            return ErrorCodes.INVALID_PIECE
+            return ReturnCodes.INVALID_PIECE
 
         if dp.get_color() == self.current_player:
-            return ErrorCodes.INVALID_MOVE
+            return ReturnCodes.INVALID_MOVE
 
-        old_board = self.board
+        old_board = self.board.copy()
         err = check_move(p, src, dest)
 
-        if err != ErrorCodes.NONE:
+        if err > ReturnCodes.CASTLE_RIGHT:
             return err
+
+        #Perform the move
+        dp,sp = sp,dp
+
+        #reduce the following code at some point....
+        if err == ReturnCodes.CASTLE_LEFT:
+            second_index = dest[1]+1;
+            rook_source = Reference(self.board[src[0]][0])
+            rook_dest = Reference(self.board[dest[0]][dest[1]+1])
+            rook_dest, rook_source = rook_source, rook_dest
+            rook_dest.castle_possible = False
+
+        if err == ReturnCodes.CASTLE_RIGHT:
+            second_index = dest[1]-1;
+            rook_source = Reference(self.board[src[0]][0])
+            rook_dest = Reference(self.board[dest[0]][dest[1]-1])
+            rook_dest, rook_source = rook_source, rook_dest
+            rook_dest.castle_possible = False
+
+        #update metadata if needed
+        if type(dp) == type(King): dp.castle_possible=False
+        if type(dp) == type(Rook): dp.castle_possible=False
+
 
         #Sanity check on putting yourself in check...
         err = check_check()
-        if err != ErrorCodes.NONE:
-            self.board = old_board
+        if err != ReturnCodes.NONE:
+            self.board = old_board.copy()
             return err
 
         #We have finished the move sequence swap players
         swap_player()
 
-        return ErrorCodes.NONE
+        return ReturnCodes.NONE
 
     def parse_move(self, move):
         if len(move) != 4:
-            return 0,0,ErrorCodes.INVALID_MOVE_LENGTH
+            return 0,0,ReturnCodes.INVALID_MOVE_LENGTH
 
         #The first letter is between a and h
         #The second character is a number between 1 and 8
@@ -135,12 +164,12 @@ class Chess:
 
         #Check validity of the move locations
         if not all(i >= 0 and i <8 for i in src):
-            return 0,0,ErrorCodes.OUT_OF_BOUNDS_START_LOCATION
+            return 0,0,ReturnCodes.OUT_OF_BOUNDS_START_LOCATION
 
         if not all(i >= 0 and i <8 for i in dest) or src == dest:
-            return 0,0,ErrorCodes.OUT_OF_BOUNDS_END_LOCATION
+            return 0,0,ReturnCodes.OUT_OF_BOUNDS_END_LOCATION
 
-        return src, dest, ErrorCodes.NONE
+        return src, dest, ReturnCodes.NONE
 
     def check_check(self):
         pass
@@ -154,7 +183,7 @@ class Chess:
     #This may not be possible without having other returns to allow for moves like castle and en passant
     #Sigh...castle and en passant
 
-    def check_move(self, p : Rook, src, dest) -> ErrorCodes:
+    def check_move(self, p : Rook, src, dest) -> ReturnCodes:
         #check to see if one axis is constant between src and dest
         #then check to see there are no pieces in the way
         if src[0] == dest[0]:
@@ -162,7 +191,7 @@ class Chess:
         elif src[1] == dest[1]:
             incr = (1,0) if src[0] < dest[0] else (-1,0)
         else:
-            return ErrorCodes.INVALID_MOVE
+            return ReturnCodes.INVALID_MOVE
 
         #Now iterate from the src to the dest using the increment operator and make sure nothing is blocking.
         tmp = src
@@ -171,19 +200,16 @@ class Chess:
             if self.board[tmp[0]][tmp[1]].get_color() != Piece.Color.NONE:
                 break
         if tmp != dest:
-            return ErrorCodes.INVALID_MOVE
+            return ReturnCodes.INVALID_MOVE
 
-        self.board[dest[0]][dest[1]] = p
-        self.board[src[0]][src[1]] = Empty()
+        return ReturnCodes.NONE
 
-        return ErrorCodes.NONE
-
-    def check_move(self, p : Bishop, src, dest) -> ErrorCodes:
+    def check_move(self, p : Bishop, src, dest) -> ReturnCodes:
         #check to make sure the slope is either 1 or -1
         num = src[0] - dest[0]
         denom = src[1] - dest[1]
         if not (num == denom or num == -denom):
-            return ErrorCodes.INVALID_MOVE
+            return ReturnCodes.INVALID_MOVE
 
         #we increment by some combination of 1 and -1 given the slope
         incr = (1 if num > 0 else -1 , 1 if denom > 0 else -1)
@@ -195,65 +221,52 @@ class Chess:
             if self.board[tmp[0]][tmp[1]].get_color() != Piece.Color.NONE:
                 break
         if tmp != dest:
-            return ErrorCodes.INVALID_MOVE
+            return ReturnCodes.INVALID_MOVE
 
-        self.board[dest[0]][dest[1]] = p
-        self.board[src[0]][src[1]] = Empty()
+        return ReturnCodes.NONE
 
-        return ErrorCodes.NONE
-
-    def check_move(self, p : Queen, src, dest) -> ErrorCodes:
+    def check_move(self, p : Queen, src, dest) -> ReturnCodes:
         old_board = self.board
 
-        if check_move(Bishop(self.current_player), src, dest) == ErrorCodes.NONE:
-            self.board[dest[0]][dest[1]] = p
-            return ErrorCodes.NONE
+        if check_move(Bishop(self.current_player), src, dest) == ReturnCodes.NONE:
+            return ReturnCodes.NONE
 
         self.board = old_board
         if check_move(Rook(self.current_player, castle_possible=False), src, dest) == ErrorCoes.NONE:
-            self.board[dest[0]][dest[1]] = p
-            return ErrorCodes.NONE
-        return ErrorCodes.INVALID_MOVE
+            return ReturnCodes.NONE
 
-    def check_move(self, p: Knight, src, dest) -> ErrorCodes:
+        return ReturnCodes.INVALID_MOVE
+
+    def check_move(self, p: Knight, src, dest) -> ReturnCodes:
         #check to see if the destination is either (+-1,+-2) or (+-2, +-1) away
         dy = src[0] - dest[0]
         dx = src[1] - dest[1]
         poss = [-1, 1, -2, 2]
         if not ((dx in poss) and (dy in poss) and (dx != dy)):
-            return ErrorCodes.INVALID_MOVE
+            return ReturnCodes.INVALID_MOVE
 
-        self.board[dest[0]][dest[1]] = p
-        self.board[src[0]][src[1]] = Empty()
+        return ReturnCodes.NONE
 
-        return ErrorCodes.NONE
+    def check_move(self, p: King, src, dest) -> ReturnCodes:
 
-    def check_move(self, p: King, src, dest) -> ErrorCodes:
-
-        """
         #first check if the move is a castle
         old_board = self.board
         if p.castle_possible:
             #if moving left check if there's a rook in the left corner
             #if moving right check if there's a rook on the right side
-            if dest[1] - src[1] == -2: #moving left
-                if type(self.board[src[0]][0]) == type(Rook) and self.board[src[0]][0].castle_possible == True:
-                    #move the king towards the spot checking for check each time
-
-
-
-            if dest[1] - src[1] == 2: #moving right
-                if type(self.board[src[0]][7]) == type(Rook) and self.board[src[0]][7].castle_possible == True:
-        """
+            second_index = 0 if dest[1] > src[1] else 7
+            inbetween = True
+            #f for file (column) r for rank (row)
+            for f in xrange(src[0], secondIndex+1, 1 if secondIndex > src[0] else -1):
+                inbetween = inbetween and self.board[src[0]][f].get_color() == Piece.Color.NONE
+            if inbetween and (type(self.board[src[0]][second_index]) == type(Rook) and self.board[src[0]][secondIndex].castle_possible == True):
+                return ReturnCodes.CASTLE_LEFT if secondIndex == 0 else ReturnCodes.CASTLE_RIGHT
 
         #now check to see if the move is possible otherwise
         dy = dest[0] - src[0]
         dx = dest[1] - src[1]
         if not (dy in [1,-1] and dx in [1,-1]):
-            return ErrorCodes.NONE
-
-        self.board[dest[0]][dest[1]] = p
-        self.board[src[0]][src[1]] = Empty()
+            return ReturnCodes.NONE
 
     def check_move(self, p: Pawn, src, dest):
         if p.two_rank_possible and src[1] == dest[1] and (dest[0] - src[0] in [2,-2]):
